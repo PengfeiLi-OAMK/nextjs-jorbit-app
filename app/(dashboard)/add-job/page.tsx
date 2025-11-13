6762f9c629879bbb219e826bb97ac20fab4c9eec
PengfeiLi-OAMK <t3lipe01@students.oamk.fi>
2025-11-13 22:58:31 +0200

    complete ' create job form' on add-job page

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import CreateJobForm from "@/components/CreateJobForm"
 
 function AddJobPage() {
-  return <h1 className='text-4xl'>AddJobPage</h1>
+  return <CreateJobForm/>
   
 }
 
